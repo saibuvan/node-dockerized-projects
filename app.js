81d57db4fc9902038927570060b2cd51cd28bc86
saibuvan <buvaneshganesan29@gmail.com>
2025-10-14 05:13:50 +0000

    serve s

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const port = 3001;
 
 // Basic route
 app.get('/', (req, res) => {
+  res.send('Hello, Express server is running!');
 });
 
 // Start server
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`🚀 Server running at http://localhost:${port}`);
 });
+
