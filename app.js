1d00fb4e096ad60ad26866f6ed4d5420c0d1f445
saibuvan <buvaneshganesan29@gmail.com>
2025-10-15 10:58:07 +0000

    staging server

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-// Basic route
 app.get('/', (req, res) => {
   res.send('Hello, Express server is running!');
 });
 
-// Start server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
