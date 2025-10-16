91621f3569f3d0811418b90a044d0d414d9e5df4
saibuvan <buvaneshganesan29@gmail.com>
2025-10-16 05:53:56 +0000

    everything-3

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3002;
 
-// Route
 app.get('/', (req, res) => {
   res.send('Node.js Buvanesh service is running!');
 });
 
-// ✅ Listen only once
 app.listen(port, '0.0.0.0', () => {
   console.log(`🚀 Server running at http://localhost:${port}`);
 });
