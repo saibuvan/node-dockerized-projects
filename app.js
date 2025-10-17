627a28ee137ea27358b739063f084947418d52f8
saibuvan <buvaneshganesan29@gmail.com>
2025-10-17 06:14:55 +0000

    develop se

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 3002;
 
 app.get('/', (req, res) => {
-  res.send('Node.js Buvanesh service js guys');
-  res.send('jenkins server apps service!');
+  res.send('Node.js service is running!!!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`🚀 Server running at http://localhost:${port}`);
 });
