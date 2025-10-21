ea7004c6f77c364a3355fcee3fce5a052e72bca7
saibuvan <buvaneshganesan29@gmail.com>
2025-10-21 08:23:39 +0000

    changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3002;
+const port = 3001;
 
 app.get('/', (req, res) => {
   res.send('Node.js service is running my application!!!');
