6d363265a21f7ef9c447783698c960b63b981d0f
saibuvan <buvaneshganesan29@gmail.com>
2025-10-22 12:40:37 +0000

    every-okay

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port =  3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Redington!');
+  res.send('Hello Redington application working fine!');
 });
 
 app.listen(port, '0.0.0.0', () => {
