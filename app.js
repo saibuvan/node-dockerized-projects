a8cc3044bc84ff1281b83d1bba5075f65291f3c5
Buvanesh <buvanesh.ganeshan@redingtongroup.com>
2025-10-22 14:41:53 +0530

    hello redington-1

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 3002;
+const port = 8082;
 
 app.get('/', (req, res) => {
-  res.send('Node.js service is running my application!!!');
+  res.send('hello redington!');
 });
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`🚀 Server running at http://localhost:${port}`);
+  res.send('Hello, Express server is running!');
 });
 
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
 
