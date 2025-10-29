b71559eae325fc5a1080de91dd7b7ec1b4952d26
Buvanesh <buvanesh.ganeshan@redingtongroup.com>
2025-10-29 10:21:02 +0530

    staging update-42

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,23 @@
-# Display the Docker container ID
 output "container_id" {
   description = "ID of the created Docker container"
   value       = docker_container.node_app_container.id
 }
 
-# Display the Docker container name
 output "container_name" {
   description = "Name of the Docker container"
   value       = docker_container.node_app_container.name
 }
 
-# Display the Docker image used
 output "image_used" {
-  description = "Docker image used for this container"
+  description = "Docker image used for the container"
   value       = docker_image.node_app_image.name
 }
 
-# Display all exposed ports (App, HTTP, SSH)
 output "exposed_ports" {
   description = "Ports exposed from the container"
-  value = {
-    app_port  = var.host_port
-    http_port = 80
-    ssh_port  = 22
-  }
+  value = [
+    "3000 (Node App)",
+    "80 (HTTP)",
+    "22 (SSH)"
+  ]
 }
