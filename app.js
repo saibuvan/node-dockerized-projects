54a02eec00fc6405dc14fa4dfbddbe830b549d7b
Buvanesh <buvanesh.ganeshan@redingtongroup.com>
2025-10-29 14:58:54 +0530

    staging update-51

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 
-// Use env PORT if provided, fallback to 3005
-const port =  3000;
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World Redington Service!!!');
 });
 
+// ✅ Health endpoint
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'UP', message: 'Service is healthy' });
+});
+
 app.listen(port, '0.0.0.0', () => {
-        console.log(`My Sample Application`);
   console.log(`🚀 Server running at http://localhost:${port}`);
 });
