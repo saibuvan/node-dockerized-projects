e4747e878ae62267744ce295a5cff50b82a87329
Buvanesh <buvanesh.ganeshan@redingtongroup.com>
2025-10-30 11:30:54 +0530

    staging update-54

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World Redington Service!!!');
+  res.send('Hello World my Redington application!!!');
 });
 
 // ✅ Health endpoint
