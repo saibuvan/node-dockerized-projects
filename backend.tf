36f747847ab8c094a9a6e721e341b47454dba0f9
saibuvan <buvaneshganesan29@gmail.com>
2025-11-03 09:27:49 +0000

    myser

--- backend.tf
+++ backend.tf
@@ -1,11 +1,15 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state"
-    key            = "state/terraform.tfstate"
-    region         = "us-east-1"
-    endpoint       = "http://localhost:9000"
-    access_key     = "minioadmin"
-    secret_key     = "minioadmin"
+    bucket                      = "terraform-state"
+    key                         = "node-app/terraform.tfstate"
+    region                      = "us-east-1"
+    endpoints = {
+      s3 = "http://localhost:9000"
+    }
+
+    access_key                  = "minioadmin"
+    secret_key                  = "minioadmin"
+
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
--- provider.tf
+++ provider.tf
@@ -5,7 +5,10 @@ provider "aws" {
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
-  endpoints {
+  s3_force_path_style         = true
+
+  endpoints = {
     s3 = "http://localhost:9000"
   }
 }
+
