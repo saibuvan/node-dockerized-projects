8a9124c4a5bead6bd0d8f1793b3af0d71325136f
saibuvan <buvaneshganesan29@gmail.com>
2025-11-03 08:25:43 +0000

    everythinh

--- backend.tf
+++ backend.tf
@@ -1,3 +1,14 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "terraform-state"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    endpoint       = "http://localhost:9000"
+    access_key     = "minioadmin"
+    secret_key     = "minioadmin"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    force_path_style            = true
+  }
 }
--- provider.tf
+++ provider.tf
@@ -1,18 +1,11 @@
 provider "aws" {
+  region                      = "us-east-1"
   access_key                  = "minioadmin"
   secret_key                  = "minioadmin"
-  region                      = "us-east-1"
-
-  # Point to MinIO instead of AWS
-  endpoints = {
-    s3 = "http://localhost:9000"
-  }
-
-  # Disable all AWS API calls
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
-
-  # Force MinIO path-style (important)
-  s3_force_path_style = true
+  endpoints {
+    s3 = "http://localhost:9000"
+  }
 }
