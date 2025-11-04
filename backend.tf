917feadba2c01f9457de5a7915e3dec78b96ee90
Buvanesh <buvanesh.ganeshan@redingtongroup.com>
2025-11-04 09:40:53 +0530

    servee-10

--- backend.tf
+++ backend.tf
@@ -6,13 +6,13 @@ terraform {
     endpoints = {
       s3 = "http://localhost:9000"
     }
-#servie files ser
+#servie files server
     access_key                  = "minioadmin"
     secret_key                  = "minioadmin"
 
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
-    force_path_style            = true
+    use_path_style              = true
   }
 }
