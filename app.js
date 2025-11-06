10c036037b4ae274b5d1e61e1b8569c5d0985d4f
saibuvan <buvaneshganesan29@gmail.com>
2025-11-06 04:41:25 +0000

    noway

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World my Redington services buddy forever nothing friend!!!');
+  res.send('Hello World my Redington services buddy forever nothing yeah!!!');
 });
 
 // ✅ Health endpoint
