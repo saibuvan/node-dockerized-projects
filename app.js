a32fe1064cbd01e7cf64451ef6a58554b1ae6742
saibuvan <buvaneshganesan29@gmail.com>
2025-11-06 04:26:50 +0000

    jenkins service-1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World my Redington services buddy forever nothing guy!!!');
+  res.send('Hello World my Redington services buddy forever nothing friend!!!');
 });
 
 // ✅ Health endpoint
